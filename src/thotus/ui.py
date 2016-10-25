c177d10cd77913c6939b79fe6667b0fc892f0cc5
fab <fdev31@gmail.com>
2016-10-25 21:08:46 +0200

    ui supports multi-window

--- src/thotus/ui.py
+++ src/thotus/ui.py
@@ -5,24 +5,30 @@ import cv2
 from scipy.misc import imresize
 
 class GUI:
+
     name = 'Thot display'
+
     def __init__(self):
         cv2.namedWindow(self.name)
+        self.secondary = []
 
     def progress(self, text, val, total=100):
         print("\r%s [%d] @ %3d%%"%(text, val, int(100.0*val/total)), end='')
         sys.stdout.flush()
 
     def clear(self):
-        cv2.destroyWindow(self.name)
-        cv2.imshow(self.name, 0)
+        names = [self.name] + self.secondary
+        for name in names:
+            cv2.destroyWindow(name)
+            cv2.imshow(name, 0)
+        self.secondary.clear()
         self._wk()
 
     def _wk(self):
         while cv2.waitKey(10) > 0:
             pass
 
-    def display(self, image, text, resize=None):
+    def display(self, image, text, resize=None, disp_number=0):
         if resize:
             image = image.copy()
         if text:
@@ -35,7 +41,13 @@ class GUI:
 
         if resize:
             image = imresize(image, resize)
-        cv2.imshow(self.name, image)
+        if disp_number:
+            name = "%s %d"%(self.name, disp_number)
+            cv2.imshow(name, image)
+            if not name in self.secondary:
+                self.secondary.append(name)
+        else:
+            cv2.imshow(self.name, image)
         self._wk()
 
 gui = GUI()
