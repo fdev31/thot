8d3854eacb3346fce8dec21f45a1713e553d4358
fab <fdev31@gmail.com>
2016-10-28 14:22:04 +0200

    fix indent

--- src/thotus/algorithms/algo_pureimage.py
+++ src/thotus/algorithms/algo_pureimage.py
@@ -3,29 +3,29 @@ from thotus.algorithms.ransac import ransac
 import numpy as np
 import cv2
 
-def compute(img, laser_nr, threshold=40, use_ransac=False):
-	x = []
-	y = []
-	img = cv2.blur(img, (5, 5))
+def compute(img, laser_nr, threshold=1, use_ransac=False):
+    x = []
+    y = []
+    img = cv2.blur(img, (5, 5))
 
-	bound = 0 if laser_nr == 0 else -1
-	cur_chunk = []
-	for n in range(img.shape[0]):
-		max_val = np.max(img[n])
-		if max_val < threshold:
-			continue
-		peaks = np.where(img[n] == max_val)[0]
-		y.append(n)
-		x.append(peaks[bound])
+    bound = 0 if laser_nr == 0 else -1
 
-	y = np.array(y)
-	x = np.array(x)
+    for n in range(img.shape[0]):
+        max_val = np.max(img[n])
+        if max_val < threshold:
+            continue
+        peaks = np.where(img[n] == max_val)[0]
+        y.append(n)
+        x.append(peaks[bound])
 
-	if use_ransac:  # line calibration
-		x = ransac( x, y )
+    y = np.array(y)
+    x = np.array(x)
 
-	points = (x, y)
-	if points:
-		return (points, compute_line_image(points, img))
+    if use_ransac:  # line calibration
+        x = ransac( x, y )
+
+    points = (x, y)
+    if points:
+        return (points, compute_line_image(points, img))
     return None, None
 
