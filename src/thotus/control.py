923903fef03c13f495c36a756443875a97d80019
fab <fdev31@gmail.com>
2016-10-31 11:18:18 +0100

    Simplify delay code

--- src/thotus/control.py
+++ src/thotus/control.py
@@ -75,8 +75,6 @@ def scan(kind=ALL, definition=1, angle=360, calibration=False, on_step=None, dis
     s.lasers_off()
     s.current_rotation = 0
 
-    IO_DELAY = 0.15/2
-
     for n in range(angle):
         if definition > 1 and n%definition != 0:
             continue
@@ -86,20 +84,16 @@ def scan(kind=ALL, definition=1, angle=360, calibration=False, on_step=None, dis
         t0 = time()
         if on_step:
             on_step()
-        t = time() - t0
-        sleep(max(0, (definition*0.1)-t)) # wait for motor
-        s.wait_capture(2)
+        s.wait_capture(2, minus=time()-t0, min_val=(definition-1)*0.2)
         if kind & COLOR:
             disp( s.save('color_%03d.%s'%(n, settings.FILEFORMAT)) , '')
         if kind & LASER1:
             s.laser_on(0)
-            sleep(IO_DELAY)
             s.wait_capture(2)
             disp( s.save('laser0_%03d.%s'%(n, settings.FILEFORMAT)), 'laser 1')
             s.laser_off(0)
         if kind & LASER2:
             s.laser_on(1)
-            sleep(IO_DELAY)
             s.wait_capture(2)
             disp( s.save('laser1_%03d.%s'%(n, settings.FILEFORMAT)) , 'laser 2')
             s.laser_off(1)
