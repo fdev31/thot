Fixes for the ciclop scanner + set UI scaling global

src/thotus/cloudify.py
@@ -76,7 +76,7 @@ def iter_cloudify(calibration_data, folder, lasers, sequence, rotated=False, met
                 if interactive:
                     disp = cv2.merge( np.array(( laser_grey, processed, processed)) )
                     txt = "Esc=NOT OK, Enter=OK"
-                    gui.display(disp, txt,  resize=0.7)
+                    gui.display(disp, txt,  resize=settings.UI_RATIO)
                 pictures_todisplay.append((processed, laser_grey))
                 if interactive:
                     if not gui.ok_cancel(20):
@@ -105,12 +105,12 @@ def iter_cloudify(calibration_data, folder, lasers, sequence, rotated=False, met
                 r = cv2.bitwise_or(gref, nref)
                 disp = cv2.merge( np.array(( r, gref, r)) )
 
-                gui.display(disp, "lasers" if len(lasers) > 1 else "laser %d"%lasers[0],  resize=0.7)
+                gui.display(disp, "lasers" if len(lasers) > 1 else "laser %d"%lasers[0],  resize=settings.UI_RATIO)
             else:
                 if len(pictures_todisplay) > 1:
-                    gui.display(cv2.addWeighted(pictures_todisplay[1][1], 0.5, pictures_todisplay[0][1], 0.5, 0), "lasers" if len(lasers) > 1 else "laser %d"%lasers[0],  resize=0.7)
+                    gui.display(cv2.addWeighted(pictures_todisplay[1][1], 0.5, pictures_todisplay[0][1], 0.5, 0), "lasers" if len(lasers) > 1 else "laser %d"%lasers[0],  resize=settings.UI_RATIO)
                 else:
-                    gui.display(pictures_todisplay[0][1], "lasers" if len(lasers) > 1 else "laser %d"%lasers[0],  resize=0.7)
+                    gui.display(pictures_todisplay[0][1], "lasers" if len(lasers) > 1 else "laser %d"%lasers[0],  resize=settings.UI_RATIO)
         else:
             gui.redraw()
     if len(sliced_lines) == 0:

src/thotus/commands.py
@@ -35,7 +35,7 @@ def scan(kind=ALL, definition=1, angle=360, calibration=False, on_step=None, dis
     s = get_scanner()
     if display:
         def disp(img, text):
-            gui.display(np.rot90(img, 3), text=text, resize=(640,480))
+            gui.display(np.rot90(img, 3), text=text, resize=settings.UI_RATIO)
     else:
         def disp(*a):
             return
@@ -58,11 +58,11 @@ def scan(kind=ALL, definition=1, angle=360, calibration=False, on_step=None, dis
         if on_step:
             on_step()
 
-        s.wait_capture(ftw,
-                minus=time()-t0,
-                min_val=0.1
-                )
+        if calibration:
+            sleep(0.05*definition)
+
         if kind & COLOR:
+            s.wait_capture(ftw)
             disp( s.save('color_%03d.%s'%(n, settings.FILEFORMAT)) , '')
         if kind & LASER1:
             s.laser_on(0)
@@ -144,7 +144,10 @@ class Viewer(Thread):
 
         while self.running:
             s.wait_capture(1)
-            img = np.rot90(s.cap.buff, settings.ROTATE)
+            if settings.ROTATE:
+                img = np.rot90(s.cap.buff, 3)
+            else:
+                img = s.cap.buff
             grey = img[:,:,1]
             found, corners = chess_detect(grey)
             if found: