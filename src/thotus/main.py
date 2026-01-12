86ae1fec0dc3848d47a12771483d7ef1333eb721
fdev31 <fdev31@gmail.com>
2026-01-12 22:04:12 +0100

    modernize: switch to poetry / pyprojects

--- src/thotus/main.py
+++ src/thotus/main.py
@@ -18,13 +18,15 @@ from thotus.calibration.chessboard import chess_detect, chess_draw
 from thotus.ui import gui
 from thotus import settings
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv("DEBUG", False)
+
 
 def s2h(t):
     if t > 80:
-        return "%d min %ds"%divmod(t, 60)
+        return "%d min %ds" % divmod(t, 60)
     else:
-        return "%.1fs"%t
+        return "%.1fs" % t
+
 
 class MainGUi:
     running = True
@@ -47,19 +49,23 @@ class MainGUi:
             if settings.ROTATE:
                 img = np.ascontiguousarray(np.rot90(img, settings.ROTATE))
             if self.line_mode:
-                lineprocessor = getattr(lm, 'from_'+settings.SEGMENTATION_METHOD)
+                lineprocessor = getattr(lm, "from_" + settings.SEGMENTATION_METHOD)
                 s.lasers_on()
                 laser_image = s.cap.get(1)
                 if settings.ROTATE:
-                    laser_image = np.ascontiguousarray(np.rot90(laser_image, settings.ROTATE))
+                    laser_image = np.ascontiguousarray(
+                        np.rot90(laser_image, settings.ROTATE)
+                    )
                 s.lasers_off()
-                points, processed = lineprocessor(laser_image, laser_image[:,:,0], img, img[:,:,0])
+                points, processed = lineprocessor(
+                    laser_image, laser_image[:, :, 0], img, img[:, :, 0]
+                )
                 if processed is None:
-                    pass # img = black picture ??
+                    pass  # img = black picture ??
                 else:
                     img = processed
             else:
-                grey = img[:,:,1]
+                grey = img[:, :, 1]
                 found, corners = chess_detect(grey)
                 if found:
                     chess_draw(img, found, corners)
@@ -76,12 +82,12 @@ class MainGUi:
                 return
 
     async def wait_interval(self):
-        await asyncio.sleep(1/60 if self.visible else 1)
+        await asyncio.sleep(1 / 60 if self.visible else 1)
 
     async def cli(self):
         script_commands = []
-        if len(sys.argv) > 2 and sys.argv[1] == 'exec':
-            script_commands.extend(x.strip() for x in ' '.join(sys.argv[2:]).split(','))
+        if len(sys.argv) > 2 and sys.argv[1] == "exec":
+            script_commands.extend(x.strip() for x in " ".join(sys.argv[2:]).split(","))
             toggle_advanced_mode()
         session = prompt_toolkit.PromptSession()
 
@@ -90,7 +96,12 @@ class MainGUi:
                 text = script_commands.pop(0)
             else:
                 try:
-                    text = await session.prompt_async(u'Scan> ', completer = WordCompleter(commands, ignore_case=True, match_middle=False))
+                    text = await session.prompt_async(
+                        "Scan> ",
+                        completer=WordCompleter(
+                            list(commands), ignore_case=True, match_middle=False
+                        ),
+                    )
                 except CancelledError:
                     return
                 except EOFError:
@@ -105,7 +116,7 @@ class MainGUi:
             if self.running:
                 if text.strip():
                     orig_text = text
-                    if ' ' in text:
+                    if " " in text:
                         params = text.split()
                         text = params[0]
                         params = [x.strip() for x in params[1:]]
@@ -113,7 +124,9 @@ class MainGUi:
                         params = ()
                     text = text.strip()
                     if text == "exec":
-                        script_commands[:] = [x.strip() for x in ' '.join(params).split(',') if x.strip()]
+                        script_commands[:] = [
+                            x.strip() for x in " ".join(params).split(",") if x.strip()
+                        ]
                         continue
                     try:
                         if text == "exit":
@@ -128,7 +141,7 @@ class MainGUi:
                         gui.clear()
                         print("\nAborted!")
                     except KeyError:
-                        print("Command not found: %s"%text)
+                        print("Command not found: %s" % text)
                     except Exception as e:
                         gui.clear()
                         print("")
@@ -139,7 +152,7 @@ class MainGUi:
                     else:
                         duration = time() - start_execution_ts
                         if duration > 1:
-                            print("Command %s executed in %s"%(text, s2h(duration)))
+                            print("Command %s executed in %s" % (text, s2h(duration)))
 
     async def maincoro(self):
         self._cli = self.cli()
@@ -153,12 +166,16 @@ class MainGUi:
             traceback.print_exc()
         self.running = False
         self._coro.cancel()
-        commands['exit']()
+        commands["exit"]()
+
 
 if __name__ == "__main__":
     app = MainGUi()
     try:
-        asyncio.get_event_loop().run_until_complete(app.maincoro())
+        # Create a new event loop and run it
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(app.maincoro())
     except CancelledError:
         pass
     except Exception as e:
